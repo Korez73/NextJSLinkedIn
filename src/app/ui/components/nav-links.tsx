4daac7ded310e5189322017501082481eb9f6260
Jamie Weldon <jamie.weldon@outlook.com>
2024-09-11 06:26:40 -0500

    Using Tailwind CSS Apply styling to blog

--- src/app/ui/components/nav-links.tsx
+++ src/app/ui/components/nav-links.tsx
@@ -13,13 +13,13 @@ import Link from 'next/link';
 import clsx from 'clsx';
 
 const links = [
-  { name: 'Home', href: '/', icon: HomeIcon  },
+  { name: 'Home', href: '/', icon: HomeIcon },
   {
     name: 'Posts',
     href: '/blog/posts',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'About', href: '/blog/about', icon: UserGroupIcon},
+  { name: 'About', href: '/blog/about', icon: UserGroupIcon },
   { name: 'Contact', href: '/blog/contact', icon: EnvelopeIcon },
 ];
 
@@ -33,7 +33,7 @@ export default async function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-purple-100 md:flex-none md:justify-start md:p-2 md:px-3'"
+            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-purple-100 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <p className="hidden md:block">{link.name}</p>
           </Link>
